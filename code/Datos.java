725effc09b5455f94ef09ac6efdd283c868c9da3
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-10 12:04:05 -0300

    new content update

---
+++ code/Datos.java
@@ -0,0 +1,9 @@
+package code;
+public class Datos {
+    //datos primitivos 
+    public static void main(String[] args) {
+    //entero
+    byte entero = 12;
+    System.out.println("numero entero es: " + entero);    
+    }
+}
