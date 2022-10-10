be197a6482dffb2f361fe763b92c61285fb786b6
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-10 14:34:42 -0300

    new code update

--- code/Datos.java
+++ code/Datos.java
@@ -1,37 +1,38 @@
 package code;
 public class Datos {
-    //dates primitives
-    //enters
-    public static void main(String[] args) {
-    //byte (-128 a 127)
-    byte entero = 12;
-    //short (-32768 a 32767)
-    short x = 2899;
-    //int (2 mil mill)
-    int num = 2000000000;
-    //long()
-    long num2 = 2000000000 ;
-    System.out.println("numero byte es: " + entero);
-    System.out.println("numero short es: "+ x);
-    System.out.println("numero integer es: "+ num);
-    System.out.println("numero long es: "+ num2);
+        //dates primitives
+        //enters
+        public static void main(String[] args) {
+        //byte (-128 a 127)
+        byte entero = 12;
+        //short (-32768 a 32767)
+        short x = 2899;
+        //int (2 mil mill)
+        int num = 2000000000;
+        //long()
+        long num2 = 2000000000 ;
+        System.out.println("numero byte es: " + entero);
+        System.out.println("numero short es: "+ x);
+        System.out.println("numero integer es: "+ num);
+        System.out.println("numero long es: "+ num2);
 
-    //flotations(decimals)
-    float dec = 3.45f;
-    System.out.println("el numero decimal es: "+dec);
-    //character
-    char caracter = 'a';
-    System.out.println("el character es:" + caracter);
-    //booleans
-    boolean decicion = true;
-    System.out.println("el boleano es:" + decicion);
-    //No primitives dates
-    // numeerico no primitivo
-    integer num = null;
-    System.out.println("el valor es:" + num);
-    //cadena de caracteres
-    String cad = "hola como estas";
-    System.out.println(cad);
+        //flotations(decimals)
+        float dec = 3.45f;
+        System.out.println("el numero decimal es: "+dec);
+        //character
+        char caracter = 'a';
+        System.out.println("el character es:" + caracter);
+        //booleans
+        boolean decicion = true;
+        System.out.println("el boleano es:" + decicion);
+        //No primitives dates
+        // numeerico no primitivo
+        Integer inv = null;
+        System.out.println("el valor es:" + inv);
+        //cadena de caracteres
+        String cad = "hola como estas";
+        System.out.println(cad);
+        // constantes
 
     }
 }
