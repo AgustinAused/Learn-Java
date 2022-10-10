bb7fc8029307267e53d15ec24e1a067f61723668
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-10 12:26:37 -0300

    new code update

--- code/Datos.java
+++ code/Datos.java
@@ -1,9 +1,26 @@
 package code;
 public class Datos {
-    //datos primitivos 
+    //dates primitives
+    //enters
     public static void main(String[] args) {
-    //entero
+    //byte (-128 a 127)
     byte entero = 12;
-    System.out.println("numero entero es: " + entero);    
+    //short (-32768 a 32767)
+    short x = 2899;
+    //int (2 mil mill)
+    int num = 2000000000;
+    //long()
+    long num2 = 2000000000 ;
+    System.out.println("numero byte es: " + entero);
+    System.out.println("numero short es: "+ x);
+    System.out.println("numero integer es: "+ num);
+    System.out.println("numero long es: "+ num2);
+
+    //flotations(decimals)
+    float dec = 3.45f;
+    System.out.println("el numero decimal es: "+dec);
+    //character
+    char caracter = 'a';
+    System.out.println("el character es:" + caracter);
     }
 }
