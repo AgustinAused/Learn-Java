74a75033c2ec37e0ec574bd07955b0bd897917d8
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-11 16:33:17 -0300

    new code update

--- src/code/Operator.java
+++ src/code/Operator.java
@@ -1,7 +1,33 @@
 package code;
 
+import java.util.Scanner;
+
 public class Operator {
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        float numero1, numero2,suma, resta,div,multi,resto;
+        System.out.println("digite dos numeros: ");
+        numero2 = entrada.nextFloat();
+        numero1 = entrada.nextFloat();
+
+        //operations
+        suma = numero2 + numero1;
+        System.out.println("la suma es: "+ suma);
+        resta = numero1 - numero2;
+        System.out.println("la resta es: "+ resta);
+        multi = numero1 * numero2;
+        System.out.println("la multiplicacion es: "+ multi);
+        div = numero2 / numero1;
+        System.out.println("la division es: "+ div);
+        resto = numero1 % numero2;
+        System.out.println("el resto es: "+ resto);
+
+
+
+
+
+
 
     }
 }
