bffc1d39191dbdf8ef4daeb8a0c4bd7c775b0151
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-12 17:03:28 -0300

    new code update

--- src/code/Arrays.java
+++ src/code/Arrays.java
@@ -6,6 +6,15 @@ public class Arrays {
         String[] names = {"agustin","jose","maria","carla","martina","juan","catalina","benjamin","franco","josefina","nahuel","mili"};
         int[] age = {18,34,23,13,85,34,18,14,45,32,23,52};
         System.out.println("el primer valor de ambas listas es "+names[0]+" y tiene "+age[0]); // imprimir los valores de la lista
+        System.out.println("el largo del Array es "+names.length);//imprimis en consola la cantida de elementos que tiene una lsita
         //manipulacion de arrays
+        for (int i = 0;i < names.length; i++){
+            System.out.println(names[i]+" y tiene "+age[i]);
+        }
+        for (String i  : names) {
+            System.out.println(i);
+        }
+
+
     }
 }
