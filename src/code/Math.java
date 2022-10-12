fc7b31ceadc9521b1066a98f21f3506af6578542
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-12 10:48:51 -0300

    new code update

--- src/code/Math.java
+++ src/code/Math.java
@@ -3,16 +3,18 @@ package code;
 public class Math {
     public static void main(String[] args) {
         // libreria math(metodos)
-        double raiz = java.lang.Math.sqrt(9);
+        double raiz = java.lang.Math.sqrt(9);// raiz cuadrada
         System.out.println(raiz);
         double base = 6, exp = 3;
-        double res = java.lang.Math.pow(base,exp);
+        double res = java.lang.Math.pow(base,exp); //exponentes
         System.out.println(res);
         double num = 4.56;
         float nume = 3.53f;
-        long resultado = java.lang.Math.round(num);
+        long resultado = java.lang.Math.round(num);//redondeo
         int resu = java.lang.Math.round(nume);
         System.out.println(resultado);
         System.out.println(resu);
+        double aleatorio = java.lang.Math.random();//numeros randoms
+        System.out.println(aleatorio);
     }
 }
