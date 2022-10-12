2c42cfd576be425cea697337fdbc04a6220f2dca
Agustin Aused <107646044+AgustinAused@users.noreply.github.com>
2022-10-12 16:53:24 -0300

    new code update

---
+++ src/code/Arrays.java
@@ -0,0 +1,11 @@
+package code;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //declaracion de arrays
+        String[] names = {"agustin","jose","maria","carla","martina","juan","catalina","benjamin","franco","josefina","nahuel","mili"};
+        int[] age = {18,34,23,13,85,34,18,14,45,32,23,52};
+        System.out.println("el primer valor de ambas listas es "+names[0]+" y tiene "+age[0]); // imprimir los valores de la lista
+        //manipulacion de arrays
+    }
+}
