e57381de6fd0d837149495953da15ea0a1d119e0
Agustin Aused <agus.aused@gmail.com>
2023-01-21 15:19:15 -0300

    add CabecerosRespuesta

---
+++ Java/Servlets/CabecerosRespuesta/src/main/java/web/GeneracionExcelServlet.java
@@ -0,0 +1,33 @@
+
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/GeneracionExcelServlet")
+public class GeneracionExcelServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+      // indicar el tipo de respuesta al navegador 
+      response.setContentType("application/vnd.ms-excel");  
+      response.setHeader("Content-Disposition", "attachment;filename=excelEjemplo.xls");
+      // para uso profesional de excel usar poi.apache.org
+      
+      // indicar al navegador no guardar cache 
+      response.setHeader("Pragma","no-cache");
+      response.setHeader("Cache-Contropl", "no-store");
+      response.setDateHeader("Expires", -1);
+      
+      
+      // desplegar informaciona la cliente 
+      PrintWriter out = response.getWriter();
+      out.print("\tValores");
+      out.print("\t1");
+      out.print("\t2");
+      out.print("\t3");
+      out.print("\t4");
+      out.print("total\t=SUMA(b2:b5)");
+    }
+}
