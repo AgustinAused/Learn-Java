cf9a7daf57ebc6dc6c96ce0133f1cb13b9cd3f29
Agustin Aused <agus.aused@gmail.com>
2023-02-18 12:57:48 -0300

    Update Converciones.java

--- Java/JSPs/DirectivasJSP/src/main/java/utilerias/Converciones.java
+++ Java/JSPs/DirectivasJSP/src/main/java/utilerias/Converciones.java
@@ -7,7 +7,14 @@ import java.util.Date;
 public class Converciones {
     private static final String FORMATO_FECHA = "dd-MM-yyyy";
     
+    
+    // Convierte un objeto Date a String (Fecha)
     public static String format(Date fecha){
+        SimpleDateFormat formateador = new SimpleDateFormat(FORMATO_FECHA);
+        return formateador.format(fecha);
+    }
+    // Convierte un objeto String a String(Fecha)
+    public static String format(String fecha){
         SimpleDateFormat formateador = new SimpleDateFormat(FORMATO_FECHA);
         return formateador.format(fecha);
     }
